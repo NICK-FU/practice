a82fdb08cc47be04af80b0d4e99dbd7877b521e5
NICK-FU <2313328927@qq.com>
2018-03-27 16:06:53 +0800

    sc

---
+++ app/src/main/java/com/example/fxm/litepaltest/Fruit.java
@@ -0,0 +1,33 @@
+package com.example.fxm.litepaltest;
+
+/**
+ * Created by Planet on 2018/3/27.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
--- app/src/main/java/com/example/fxm/litepaltest/MainActivity.java
+++ app/src/main/java/com/example/fxm/litepaltest/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.fxm.litepaltest;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+
+    private Fruit[] fruits = {
+            new Fruit("apple",R.drawable.apple), new Fruit("banana",R.drawable.banana), new Fruit("cherry",R.drawable.cherry),
+            new Fruit("grape",R.drawable.grape), new Fruit("mango",R.drawable.mango), new Fruit("orange",R.drawable.orange),
+            new Fruit("pear",R.drawable.pear), new Fruit("pineapple",R.drawable.pineapple), new Fruit("strawberry",R.drawable.strawberry)
+    };
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
+
+    private SwipeRefreshLayout swipeReresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +59,44 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "dddd", Snackbar.LENGTH_SHORT)
+                        .setAction("give up",new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"done",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+        intFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rev);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+        swipeReresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeReresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeReresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+
+            @Override
+            public void onRefresh() {
+                Toast.makeText(MainActivity.this,"fresh done",Toast.LENGTH_SHORT).show();
+                swipeReresh.setRefreshing(false);
+            }
+        });
+    }
+
+    private void intFruits() {
+        fruitList.clear();
+        for (int i = 0; i < 50; i++ ){
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
     }
 
     @Override
