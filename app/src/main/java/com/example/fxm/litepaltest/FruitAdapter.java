542751313b736f6ab51b78234da898c7f034f982
NICK-FU <2313328927@qq.com>
2018-03-28 11:53:14 +0800

    fc

--- app/src/main/java/com/example/fxm/litepaltest/FruitAdapter.java
+++ app/src/main/java/com/example/fxm/litepaltest/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.fxm.litepaltest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -19,12 +20,30 @@ import java.util.List;
 
 public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
 
+    private static final String TAG = "FruitAdapter";
+
     private Context mContext;
 
     private List<Fruit> mFruitList;
 
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view;
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+
+
+
+
     public FruitAdapter(List<Fruit> fruitList) {
-        this.mFruitList = fruitList;
+        mFruitList = fruitList;
     }
 
     @Override
@@ -33,7 +52,19 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
             mContext = parent.getContext();
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,parent,false);
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int postion = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(postion);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+        return holder;
     }
 
     @Override
@@ -48,17 +79,4 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
         return mFruitList.size();
     }
 
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        CardView cardView;
-        ImageView fruitImage;
-        TextView fruitName;
-
-        public ViewHolder(View view) {
-            super(view);
-            cardView = (CardView) view;
-            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-            fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        }
-    }
 }
